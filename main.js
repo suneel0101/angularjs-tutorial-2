4ab34b88fe8dab4c62e6287712cf0965b808f5d6
Suneel Chakravorty <suneel0101@gmail.com>
2013-12-26 14:23:43 -0500

    basic directives

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ var myApp = angular.module("Suneelius", []);
 
 myApp.directive("superman", function () {
     return {
-        restrict: "E",
-        template: "<div>Here I am</div>"
+        // A for attr, C for class, E for element
+        restrict: "A",
+        link: function () {
+            alert("I'm awesomer!");
+        }
+    };
+});
+
+myApp.directive("flash", function () {
+    return {
+        // A for attr, C for class, E for element
+        restrict: "A",
+        link: function () {
+            alert("I'm fast");
+        }
     };
 });
