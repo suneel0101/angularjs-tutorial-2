212e5716d7802b454dceb8d58df72df991d4424b
Suneel Chakravorty <suneel0101@gmail.com>
2013-12-26 14:19:55 -0500

    writing first directive

--- main.js
+++ main.js
@@ -1,34 +1,8 @@
 var myApp = angular.module("Suneelius", []);
 
-myApp.factory('Data', function () {
-  return { message: "I'm data from a service" };
+myApp.directive("superman", function () {
+    return {
+        restrict: "E",
+        template: "<div>Here I am</div>"
+    };
 });
-
-myApp.factory("Avengers", function () {
-    var Avengers = {};
-    Avengers.cast = [
-        {name: "Clark Gregg", character: "Johny Aplesed"},
-        {name: "Johnny Depp", character: "Jack Sparrow"},
-        {name: "Clark Kent", character: "Superman"},
-        {name: "Bruce Wayne", character: "Batman"}
-    ];
-    return Avengers;
-});
-
-myApp.filter('reverse', function (Data) {
-    return function(text) {
-        return text.split("").reverse().join("") + Data.message;
-    }
-});
-
-function AvengersCtrl($scope, Avengers){
-    $scope.avengers = Avengers;
-}
-
-function FirstCtrl($scope, Data) {
-    $scope.data = Data;
-}
-
-function SecondCtrl ($scope, Data) {
-    $scope.data = Data;
-}
